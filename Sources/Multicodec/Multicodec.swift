1f7818a8572c3c459a0cddd6505cfe6e11b705fc
Brandon <32753167+btoms20@users.noreply.github.com>
2025-02-28 07:16:37 -0800

    Updated license header

--- Sources/Multicodec/Multicodec.swift
+++ Sources/Multicodec/Multicodec.swift
@@ -1,10 +1,20 @@
+//===----------------------------------------------------------------------===//
 //
-//  Multicodec.swift
-//  SwiftMulticodec
+// This source file is part of the swift-libp2p open source project
+//
+// Copyright (c) 2022-2025 swift-libp2p project authors
+// Licensed under MIT
+//
+// See LICENSE for license information
+// See CONTRIBUTORS for the list of swift-libp2p project authors
+//
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
 //
 //  Created by Teo Sartori
 //  Modified by Brandon Toms on 5/1/2022
-//
+
 import Foundation
 import VarInt
 
