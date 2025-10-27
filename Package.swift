5b823954f1b4159c434ed1e6ae3f906ebd7d4350
Brandon <32753167+btoms20@users.noreply.github.com>
2025-10-27 11:30:04 -0700

    Set swift-tools-version to 6.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.2
+// swift-tools-version:6.0
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the swift-libp2p open source project
