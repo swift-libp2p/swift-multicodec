f7aac3965427b071e39a3cbaa9b84abede8b4ed3
Brandon <32753167+btoms20@users.noreply.github.com>
2025-10-27 11:13:49 -0700

    Rearranged products and targets

--- Package.swift
+++ Package.swift
@@ -19,13 +19,13 @@ let package = Package(
     name: "swift-multicodec",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .executable(
-            name: "update-codecs",
-            targets: ["Updater"]
-        ),
         .library(
             name: "Multicodec",
             targets: ["Multicodec"]
+        ),
+        .executable(
+            name: "update-codecs",
+            targets: ["Updater"]
         ),
     ],
     dependencies: [
@@ -35,10 +35,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .executableTarget(
-            name: "Updater",
-            dependencies: []
-        ),
         .target(
             name: "Multicodec",
             dependencies: [
@@ -48,6 +44,10 @@ let package = Package(
         .testTarget(
             name: "MulticodecTests",
             dependencies: ["Multicodec"]
+        ),
+        .executableTarget(
+            name: "Updater",
+            dependencies: []
         ),
     ]
 )
